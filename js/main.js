d46e497f8e7f83122949bdd845f89caa74c86902
Andrei <zbu_04@yahoo.com>
2023-01-20 20:50:21 +0100

    modal display none after click on hamburger links

--- js/main.js
+++ js/main.js
@@ -4,6 +4,18 @@ const hamburger = document.querySelector('#nav-menu');
 
 const close = document.getElementsByClassName('close')[0];
 
+
+
+const nav = document.getElementsByClassName('nav')[0];
+const elem_nav = nav.getElementsByTagName("li").length;
+
+for (let i = 0; i < elem_nav; i++) {
+  const link = document.getElementsByClassName("nav-item")[i];
+
+  link.addEventListener('click', () => {
+    modal.style.display = "none";
+  });
+}
 hamburger.addEventListener('click', () => {
   modal.style.display = "block";
 });
